acfaaf68f3b18770a73bc73d404291e9c0b3c19a
AtlantProject <poliglotsten@gmail.com>
2021-08-05 19:35:03 +0300

    Урок 4

---
+++ Lesson_4/4_HW_1.2.py
@@ -0,0 +1,44 @@
+"""
+Написать программу расчета y и z по формулам, приведенным ниже.
+Определить разность между значениями  y и z в каждом случае.
+
+y = 2*sin^2(3Pi-2a)*cos^2(5Pi+2a)
+
+z = 1/4 - 1/4 * sin(5/2 * Pi - 8a)
+
+-------------------
+Тестовые значения:
+Grad        Rad         Y           Z           Y - Z
+a = 0       0           0           0           0
+a = 30      0.52       0.38         0.38        0
+a = 60      1.05       0.38         0.38        0
+a =90       1.57       0.000..      0.000..     0
+a = 180     3.14        0           0           0
+"""
+
+import math
+
+x = 180  # grad
+x_rad = math.radians(x)     # radians
+
+'''
+y = 2 * math.sin(3*math.pi - 2*a_rad)**2 * math.cos(5*math.pi + 2*a_rad)**2
+print('y={:.3f}'.format(y))
+
+z = 1/4 - 1/4 * math.sin(5/2 * math.pi - 8*a_rad)
+print('z={:.3f}'.format(z))
+
+print('y-z={:.3f}'.format(y-z))
+'''
+
+y = (x_rad**2 + 2*x_rad - 3 + (x_rad + 1)*math.sqrt(x_rad**2 - 9))/(x_rad**2 + 2*x_rad - 3 + (x_rad - 1)*math.sqrt(x_rad**2 - 9))
+
+# y1 = x_rad**2 + 2*x_rad - 3 + (x_rad + 1)*math.sqrt(x_rad**2 - 9)
+# y2 = x_rad**2 + 2*x_rad - 3 + (x_rad - 1)*math.sqrt(x_rad**2 - 9)
+# y = y1/y2
+print('y={:.3f}'.format(y))
+
+z = math.sqrt((x_rad + 3)/(x_rad - 3))
+print('z={:.3f}'.format(z))
+
+print('y-z={:.3f}'.format(y-z))
---
+++ Lesson_4/4_HW_2.py
@@ -0,0 +1,9 @@
+import math
+
+r = 10     # радиус
+
+S = math.pi * r ** 2
+print('S = {:.3f} квадратный метров'.format(S))
+
+P = 2 * math.pi * r
+print('P = {:.3f} метров'.format(P))
