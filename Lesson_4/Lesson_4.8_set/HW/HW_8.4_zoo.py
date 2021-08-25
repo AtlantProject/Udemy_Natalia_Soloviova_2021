a5af8485a32cb8315e27fdeefa6d63b26be0cb6f
AtlantProject <poliglotsten@gmail.com>
2021-08-25 14:15:44 +0300

    25.08.2021 Среда. ДЗ к уроку 4.8

--- Lesson_4/Lesson_4.8_set/HW/HW_8.4_zoo.py
+++ Lesson_4/Lesson_4.8_set/HW/HW_8.4_zoo.py
@@ -1,8 +1,8 @@
 # HW_8.4_zoo
 
-predators = set(['tigers', 'leons', 'wolfs', 'bears'])
-birds = set(['parrots', 'owls', 'pelicans', 'eagles', 'penguins', 'flamingos'])
-herbivores = set(['rabbits', 'elephants', 'giraffes', 'rhinos', 'roe deers'])
+predators = {'tigers', 'leons', 'wolfs', 'bears'}
+birds = {'parrots', 'owls', 'pelicans', 'eagles', 'penguins', 'flamingos'}
+herbivores = {'rabbits', 'elephants', 'giraffes', 'rhinos', 'roe deers'}
 
 all_animals = predators | birds | herbivores
 sorted_all_animals = sorted(all_animals)
