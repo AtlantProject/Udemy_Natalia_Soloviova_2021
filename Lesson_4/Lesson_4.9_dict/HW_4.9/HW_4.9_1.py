44ab737dcf11fb260e9f59a19149ab677fa2483b
AtlantProject <poliglotsten@gmail.com>
2021-08-25 19:47:33 +0300

    25.08.2021 Среда. ДЗ к уроку 4.9

---
+++ Lesson_4/Lesson_4.9_dict/HW_4.9/HW_4.9_1.py
@@ -0,0 +1,9 @@
+d = {'v': 's001', 'v1': 's002', 'v2': 's001', 'v3': 's005', 'v4': 's005', 'v5': 's009', 'v6': 's007' }
+
+print(d)
+
+values = d.values()
+values = set(values)
+print(values)
+sort_values = sorted(values)
+print(sort_values)
