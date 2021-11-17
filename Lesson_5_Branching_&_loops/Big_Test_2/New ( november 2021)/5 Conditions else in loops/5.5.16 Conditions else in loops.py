e38297c4ff424631f82a23879233a957ac654472
AtlantProject <poliglotsten@gmail.com>
2021-11-17 19:00:18 +0300

    17.11.2021 Контрольная работа 2. Повторение материала после отпуска

---
+++ Lesson_5_Branching_&_loops/Big_Test_2/New ( november 2021)/5 Conditions else in loops/5.5.16 Conditions else in loops.py
@@ -0,0 +1,26 @@
+'''
+Тема 5: “Условие else в циклах”
+3.	Дан список продуктов с ценами на них: ‘milk’ - $12, ‘bread’ - $10, ‘meat’ - $60, ‘vegetable mix’ - $20, ‘fruit mix’
+- $35, ‘fish’ - $45, ‘eggs’ - $15, ‘cake’ - $15. У Марты есть $100. Какое максимальное количество продуктов она может
+купить? Выведите на экран список этих продуктов. В случае если Марта сможет купить все продукты из списка, выведите
+соответствующее сообщение. Решите эту же задачу для Алекса, у которого есть $250.
+---------------
+
+'''
+goods = {'milk': 12, 'bread': 10, 'meat': 60, 'vegetable_mix': 20, 'fruit_mix': 35, 'fish': 45, 'eggs': 15, 'cake': 15}
+
+goods_sort = {'bread': 10, 'milk': 12, 'eggs': 15, 'cake': 15, 'vegetable_mix': 20, 'fruit_mix': 35, 'fish': 45, 'meat': 60}
+
+sum = 250
+max_count = 0
+
+for name, price in goods_sort.items():
+    sum -= price
+    if sum < 0:
+        print(f"\n{max_count}")
+        break
+
+    print(name, '\n', end=" ")
+    max_count += 1
+else:
+    print('\nВсе товары куплены')
