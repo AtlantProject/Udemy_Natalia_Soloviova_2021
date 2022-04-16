971780d726c3bdb0b8e01b1708382d6adf204787
AtlantProject <86153003+AtlantProject@users.noreply.github.com>
2022-04-16 18:08:40 +0300

    Test 6 modules

--- Lesson_9 modules/Les_9 Test 6/Keys/8.1 1.py
+++ Lesson_9 modules/Les_9 Test 6/Keys/8.1 1.py
@@ -9,8 +9,25 @@ print('+'*100)
 print(dir(texttable))
 print('+'*100)
 
+# специальные атрибуты
+
 print(f"Name - {texttable.__name__}")
 print('+'*100)
 print(texttable.__doc__)
 print('+'*100)
 print(texttable.__file__)
+
+# дополнительные атрибуты
+
+print(f"Author - {texttable.__author__}")
+print('+'*100)
+# print(f"Author - {texttable.__copyright__}")
+print(f"Author - {texttable.__credits__}")
+print('+'*100)
+print(f"Author - {texttable.__license__}")
+print('+'*100)
+print(f"Author - {texttable.__version__}")
+print('+'*100)
+# print(f"Author - {texttable.__maintainer__}")
+# print(f"Author - {texttable.__email__}")
+# print(f"Author - {texttable.__status__}")
